bc38f53fadb90c99e04af4500f876ad232a725a2
aakashDeveloper <ahanda205@gmail.com>
2019-12-09 03:18:56 +0000

    a

--- inbulit/app.js
+++ inbulit/app.js
@@ -12,5 +12,5 @@ var server = http.createServer(function(req,res){
 })
 
 server.listen(port, function(){
-    console.log("Server is running on port "+port)
+    console.log("Server is running on port number"+port)
 })
