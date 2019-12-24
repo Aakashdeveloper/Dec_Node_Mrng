d34aa144abe69f128b1e25e9d7659a8b4d25bdc4
aakashDeveloper <ahanda205@gmail.com>
2019-12-24 03:14:10 +0000

    a

--- jwt/auth/AuthController.js
+++ jwt/auth/AuthController.js
@@ -11,6 +11,50 @@ router.use(bodyParser.urlencoded({extended: false}));
 router.use(bodyParser.json());
 const User = require('../user/User');
 
+router.post('/register', (req,res) => {
+    const hashedPassword = bcrypt.hashSync(req.body.password, 8)
+
+    User.create({
+        name:req.body.name,
+        password:hashedPassword,
+        email:req.body.email,
+        role:req.body.role
+   },(err,user) => {
+       if(err) return res.status(500).send('There was a problem registering user')
+       res.send('Registration Successfull')
+   })
+});
+
+router.post('/login', (req,res) => {
+    User.findOne({email:req.body.email},(err,user) => {
+        if(err) return res.status(500).send('Error on server');
+        if(!user) {res.send('Register first')}
+        else{
+            const passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+            if(!passwordIsValid) return res.status(401).send({auth:false,token:null})
+            var token = jwt.sign({id:user._id},config.secert,{
+                expiresIn:86400 //24 hours
+            });
+            console.log('Login success')
+            res.send({auth:true,token:token})
+        }
+    })
+});
+
+router.get('/getUser',(req,res) => {
+    var token = req.headers['x-access-token'];
+    if(!token) res.status(401).send({auth:false, message:'No Token Provided'})
+
+    jwt.verify(token, config.secert, (err,decoded) => {
+        if(err) res.status(401).send({auth:false, message:'Token not valid'})
+        User.findById(decoded.id,{password:0},(err,user) => {
+            if(err) res.status(500).send('Problem finding user');
+            if(!user) res.status(401).send('No User found')
+            res.send(user)
+        })
+    })
+})
+
 
 
 module.exports = router;
